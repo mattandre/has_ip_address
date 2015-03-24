002de9efe5ad1302845fdfc772f3b7cc732f53ec
Matt Andre <mandre@colocrossing.com>
2015-03-24 14:12:35 -0400

    Add has_ip_address_range helper

--- lib/has_ip_address.rb
+++ lib/has_ip_address.rb
@@ -69,11 +69,11 @@ module HasIPAddress
       end
 
       define_singleton_method "by_#{name}_prefix" do |prefix|
-        where(ip_address_prefix: prefix)
+        where(prefix_column => prefix)
       end
 
       define_singleton_method "by_#{name}_version" do |version|
-        where(ip_address_version: version)
+        where(version_column => version)
       end
 
       define_singleton_method "by_#{name}" do |ip_address|
@@ -87,10 +87,52 @@ module HasIPAddress
           version = ip_address.version
         end
 
-        where(ip_address_data: data).send("by_#{name}_prefix", prefix).send("by_#{name}_version", version)
+        where(data_column => data)
+          .send("by_#{name}_prefix", prefix)
+          .send("by_#{name}_version", version)
       end
 
+      define_singleton_method "before_#{name}" do |ip_address|
+        unless ip_address.is_a?(Networking::IPAddress)
+          ip_address = Networking::IPAddress.parse!(ip_address)
+        end
+
+        where("#{data_column} <= ?", ip_address.data)
+          .send("by_#{name}_version", ip_address.version)
+      end
+
+      define_singleton_method "after_#{name}" do |ip_address|
+        unless ip_address.is_a?(Networking::IPAddress)
+          ip_address = Networking::IPAddress.parse!(ip_address)
+        end
+
+        where("#{data_column} >= ?", ip_address.data)
+          .send("by_#{name}_version", ip_address.version)
+      end
     end
+
+    def has_ip_address_range(options = {})
+      name = options.fetch(:name, :ip_address)
+
+      start_name = "start_#{name}".to_sym
+      end_name = "end_#{name}".to_sym
+
+      has_ip_address(name: start_name)
+      has_ip_address(name: end_name)
+
+      define_method "#{name}_range" do
+        start_ip_address = send(start_name)
+        end_ip_address = send(end_name)
+
+        return if start_ip_address.nil? || end_ip_address.nil?
+        (start_ip_address..end_ip_address)
+      end
+
+      define_singleton_method "contains_#{name}" do |ip_address|
+        send("before_#{start_name}", ip_address).send("after_#{end_name}", ip_address)
+      end
+    end
+
   end
 end
 
